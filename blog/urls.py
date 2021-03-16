7d8dfcb6c63d02736308760d6e221aa8af2bf0be
Junior Gantin <nioperas06@gmail.com>
2021-03-16 11:47:06 +0100

    ci/cd

--- blog/urls.py
+++ blog/urls.py
@@ -1,28 +1,12 @@
 from django.urls import path
 
 from .views import BlogDeleteView
-from .views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView
+from .views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView  # noqa: E501
 
 urlpatterns = [
-    path(
-        "post/<int:pk>/delete/",
-        BlogDeleteView.as_view(),
-        name="post_delete"
-    ),
-    path(
-        "post/new/",
-        BlogCreateView.as_view(),
-        name="post_new"
-    ),
-    path(
-        "post/<int:pk>/",
-        BlogDetailView.as_view(),
-        name="post_detail"
-    ),
-    path(
-        "post/<int:pk>/edit/",
-        BlogUpdateView.as_view(),
-        name="post_edit"
-    ),
-    path("", BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),  # noqa: E501
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),  # noqa: E501
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),  # noqa: E501
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),  # noqa: E501
+    path("", BlogListView.as_view(), name="home"),  # noqa: E501
 ]
