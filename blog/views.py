8e57655357bd25052e66a1a77fa08a280a3cd270
Junior Gantin <nioperas06@gmail.com>
2021-03-20 18:32:26 +0100

    add changes

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,7 @@ class BlogUpdateView(UpdateView):
 
 
 class BlogDeleteView(DeleteView):  # new
-    model = Post
-    template_name = "post_delete.html"
-    success_url = reverse_lazy("home")
+    pass
+    # model = Post
+    # template_name = "post_delete.html"
+    # success_url = reverse_lazy("home")
