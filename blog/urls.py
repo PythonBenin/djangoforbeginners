84470c061ffd35514593a8d041cdf807bdd9b0c1
Junior Gantin <nioperas06@gmail.com>
2021-03-20 17:19:58 +0100

    fucking tests

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),  # noqa: E501
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),  # noqa: E501
     path("", BlogListView.as_view(), name="home"),  # noqa: E501
+    path("", BlogListView.as_view(), name="home"),  # noqa: E501
 ]
